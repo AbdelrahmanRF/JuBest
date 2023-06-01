df6053a6195ec25ef4b9852509499d8055d17edc
Abdalrahman Alfar <110980420+AbdulrahmanSZ@users.noreply.github.com>
2023-06-01 07:52:19 +0300

    Update levels.js

--- backend/routes/levels.js
+++ backend/routes/levels.js
@@ -168,17 +168,13 @@ router.delete('/beginner/words/:id', isLoggedIn, isAdmin, catchAsync(async (req,
       await cloudinary.uploader.destroy(public_id, { resource_type });
     }
 
-    // Get the previous word ID
-    const previousWord = await Word.findOne({ _id: { $lt: id } }, {}, { sort: { _id: -1 } });
 
     // Delete the word from the database
     await Word.findByIdAndDelete(id);
 
     req.flash('success', 'Word has been deleted.');
 
-    // Redirect to the previous word if available, or the word list if no previous word exists
-    const redirectUrl = previousWord ? `/levels/beginner/words#${previousWord._id}` : "/levels/beginner/words";
-    res.redirect(redirectUrl);
+    res.redirect(`/levels/beginner/words#${word.category}`);
   } else {
     res.render('error', { message: 'Error deleting word', err: new Error });
   }
@@ -327,17 +323,13 @@ router.delete('/beginner/sentences/:id', isLoggedIn, isAdmin, catchAsync(async (
       await cloudinary.uploader.destroy(public_id, { resource_type });
     }
 
-    // Get the previous phrase ID
-    const previousPhrase = await Phrase.findOne({ _id: { $lt: id } }, {}, { sort: { _id: -1 } });
 
     // Delete the phrase from the database
     await Phrase.findByIdAndDelete(id);
 
     req.flash('success', 'Phrase has been deleted.');
 
-    // Redirect to the previous phrase if available, or the sentence list if no previous phrase exists
-    const redirectUrl = previousPhrase ? `/levels/beginner/sentences#${previousPhrase._id}` : "/levels/beginner/sentences";
-    res.redirect(redirectUrl);
+    res.redirect( `/levels/beginner/sentences#${phrase.category}`);
   } else {
     res.render('error', { message: 'Error deleting phrase', err: new Error });
   }
